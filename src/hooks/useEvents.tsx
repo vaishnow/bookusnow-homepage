33a16ae63d46294788a03b64b87d6651e96c20f8
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-30 14:42:39 +0530

    refactor

--- src/hooks/useEvents.tsx
+++ src/hooks/useEvents.tsx
@@ -1,20 +1,22 @@
-import eventsApi from "@/api/eventsApi";
 import { useQuery } from "@tanstack/react-query";
+import eventsApi from "@/api/eventsApi";
 
 type Props = {
   type?: string;
   page?: number;
 };
 
-type Events = {
-  events: {
-    cityName: string;
-    date: string;
-    distanceKm: string;
-    eventName: string;
-    imgUrl: string;
-    weather: string;
-  }[];
+type EventDetails = {
+  cityName: string;
+  date: string;
+  distanceKm: string;
+  eventName: string;
+  imgUrl: string;
+  weather: string;
+};
+
+type EventResp = {
+  events: EventDetails[];
   page: number;
   pageSize: number;
   totalEvents: number;
@@ -25,7 +27,7 @@ const useEvents = ({ page = 1, type }: Props) => {
   const fetchEvent = () =>
     eventsApi.get("", { params: { page, type } }).then((resp) => resp.data);
 
-  const { data, error } = useQuery<Events, Error>({
+  const { data, error } = useQuery<EventResp, Error>({
     queryKey: [type, page],
     queryFn: fetchEvent,
   });
