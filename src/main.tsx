bde9ddd3216a46c83f8497112eac4b5c03704217
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-30 13:09:47 +0530

    setup QueryClient for @tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "@/App";
-import "@/index.css"
+import "@/index.css";
+
+const queryclient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryclient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
