82d341ed5dabc8b0379569127400bbe6c83247ac
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-30 17:05:54 +0530

    fetch data for upcoming events

--- src/components/HomePage/Upcoming.tsx
+++ src/components/HomePage/Upcoming.tsx
@@ -1,6 +1,29 @@
+import useEvents from "@/hooks/useEvents";
+import { getGDriveLink } from "@/utils/gDrive.utils";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { ScrollBar } from "../ui/scroll-area";
+
 const Upcoming = () => {
+  const { data } = useEvents({ type: "upcoming" });
+
   return (
-    <div>Upcoming</div>
-  )
-}
-export default Upcoming
+    <div>
+      <ScrollArea>
+        <div className="flex w-max space-x-4 p-4">
+          {data?.events.map((item) => (
+            <div className="size-36 bg-blue-600" key={item.eventName}>
+              {item.eventName}
+              <img
+                src={getGDriveLink(item.imgUrl)}
+                alt={item.eventName}
+                className="size-full"
+              />
+            </div>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" className="hidden" />
+      </ScrollArea>
+    </div>
+  );
+};
+export default Upcoming;
