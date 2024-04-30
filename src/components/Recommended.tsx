a912da0c22f9971d1eb448a1a6ac8df3fc66000e
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-30 11:32:36 +0530

    create horizontal scrollable area

--- src/components/Recommended.tsx
+++ src/components/Recommended.tsx
@@ -1,6 +1,27 @@
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
+
 const Recommended = () => {
   return (
-    <div>Recommended</div>
-  )
-}
-export default Recommended
+    <div>
+      <ScrollArea>
+        <div className="flex w-max space-x-4 p-4">
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+          <div className="size-36 bg-blue-600">card</div>
+        </div>
+        <ScrollBar orientation="horizontal" className="hidden" />
+      </ScrollArea>
+    </div>
+  );
+};
+export default Recommended;
