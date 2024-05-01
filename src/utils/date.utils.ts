e2cc8bdf7478e8a9e5b64a495918fc43c2e9a02f
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-05-01 11:45:29 +0530

    created function for formating date string

---
+++ src/utils/date.utils.ts
@@ -0,0 +1,9 @@
+export const getFormattedDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
