2fe8ecf4c876dc4ac51ee75adf970976a2d48048
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-05-02 15:48:02 +0530

    perf: set default width for drive thumbnails to 400

--- src/utils/gDrive.utils.ts
+++ src/utils/gDrive.utils.ts
@@ -1,8 +1,8 @@
-export const getGDriveLink = (link: string): string => {
+export const getGDriveLink = (link: string,width:number=400): string => {
   if (link.includes("drive.google.com")) {
     const fileId = link.match(/(?:https:\/\/)?drive\.google\.com\/(?:file\/d\/|open\?id=)([^\/?&]+)/);
 
-    if (fileId) return `https://drive.google.com/thumbnail?id=${fileId[1]}&sz=w1000`;
+    if (fileId) return `https://drive.google.com/thumbnail?id=${fileId[1]}&sz=w${width}`;
   }
 
   return link;
