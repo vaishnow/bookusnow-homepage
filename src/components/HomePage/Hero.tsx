8d0b9f180f53d6397c4575b2eb83793f787615e4
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-05-02 12:22:47 +0530

    responsive design hero section

--- src/components/HomePage/Hero.tsx
+++ src/components/HomePage/Hero.tsx
@@ -2,12 +2,12 @@ import banner from "@/assets/banner.webp";
 
 const Hero = () => {
   return (
-    <section className="h-screen bg-transparent flex w-full text-[#f3f3f3] text-center text-balance relative">
-      <div className="m-auto px-[8%] pb-40">
-        <h1 className="text-[4rem] font-semibold">
+    <section className="h-[90vh] md:h-screen bg-transparent flex w-full justify-center text-[#f3f3f3] text-center text-balance relative">
+      <div className="mt-20 px-5 max-sm:leading-loose md:px-[8%] pb-40">
+        <h1 className="text-[1.6rem] sm:text-[3rem] lg:text-[4rem] font-semibold mb-5">
           Discover Exciting Events Happening Near You – Stay Tuned for Updates!
         </h1>
-        <p className="text-xl font-extralight">
+        <p className="text-md font-light line-clamp-3 px-5">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus beatae
           aperiam facilis nihil reiciendis pariatur unde dolor
           temporibus, quo deserunt in nostrum non voluptates. Soluta deserunt
